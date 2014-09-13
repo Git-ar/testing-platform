98f7a7fe20254ce637a01accde107ae2bbcd417b
Aakash Adesara <aakash.adesara@gmail.com>
2014-09-12 20:09:26 -0700

    Added boilerplate
    Code found on Github. Made by Charles. We edited it for our needs. ALL
    THE CODE DOES is get microphone input. We are going to add our own
    algorithms to it. Once again, ALL THE ALGORITHMS ARE ORIGINAL.

---
+++ old/test.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+
+	  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        var context = new AudioContext();
+        var analyser = context.createAnalyser();
+
+ 
+});
