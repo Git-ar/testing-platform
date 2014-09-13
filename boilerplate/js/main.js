440098d625adb922c57c288fabdb6c6a53579ffe
Aakash Adesara <aakash.adesara@gmail.com>
2014-09-12 21:09:31 -0700

    Deleted unwanted stuff

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -25,64 +25,11 @@ var analyserContext = null;
 var canvasWidth, canvasHeight;
 var recIndex = 0;
 
-/* TODO:
-
-- offer mono option
-- "Monitor input" switch
-*/
-
-function saveAudio() {
+/*function saveAudio() {
     audioRecorder.exportWAV( doneEncoding );
     // could get mono instead by saying
     // audioRecorder.exportMonoWAV( doneEncoding );
-}
-
-/*function gotBuffers( buffers ) {
-    var canvas = document.getElementById( "wavedisplay" );
-    console.log("marker");
-    drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
-
-    // the ONLY time gotBuffers is called is right after a new recording is completed - 
-    // so here's where we should set up the download.
-    audioRecorder.exportWAV( doneEncoding );
 }*/
-/*
-function doneEncoding( blob ) {
-    Recorder.setupDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
-    recIndex++;
-}
-
-function toggleRecording( e ) {
-    if (e.classList.contains("recording")) {
-        // stop recording
-        audioRecorder.stop();
-        e.classList.remove("recording");
-        audioRecorder.getBuffers( gotBuffers() );
-    } else {
-        // start recording
-        if (!audioRecorder)
-            return;
-        e.classList.add("recording");
-        audioRecorder.clear();
-        audioRecorder.record();
-    }
-}
-
-function convertToMono( input ) {
-    var splitter = audioContext.createChannelSplitter(2);
-    var merger = audioContext.createChannelMerger(2);
-
-    input.connect( splitter );
-    splitter.connect( merger, 0, 0 );
-    splitter.connect( merger, 0, 1 );
-    return merger;
-}
-
-function cancelAnalyserUpdates() {
-    window.cancelAnimationFrame( rafID );
-    rafID = null;
-}
-*/
 
 // MAIN FUNCTION
 var minPt = 200;
@@ -122,12 +69,12 @@ function updateAnalysers(time) {
             analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
             analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
 
-            if(minPt > -magnitude){
-                minPt = -magnitude;
+            if(minPt > magnitude){
+                minPt = magnitude;
             }
 
-            if(maxPt < -magnitude){
-                maxPt = -magnitude;
+            if(maxPt < magnitude){
+                maxPt = magnitude;
             }
         }
     }
@@ -136,20 +83,6 @@ function updateAnalysers(time) {
     
     rafID = window.requestAnimationFrame( updateAnalysers );
 }
-/*
-function toggleMono() {
-    if (audioInput != realAudioInput) {
-        audioInput.disconnect();
-        realAudioInput.disconnect();
-        audioInput = realAudioInput;
-    } else {
-        realAudioInput.disconnect();
-        audioInput = convertToMono( realAudioInput );
-    }
-
-    audioInput.connect(inputPoint);
-}
-*/
 function gotStream(stream) {
     inputPoint = audioContext.createGain();
 
@@ -164,7 +97,7 @@ function gotStream(stream) {
     analyserNode.fftSize = 2048;
     inputPoint.connect( analyserNode );
 
-    audioRecorder = new Recorder( inputPoint );
+    //audioRecorder = new Recorder( inputPoint );
 
     zeroGain = audioContext.createGain();
     zeroGain.gain.value = 0.0;
